c8d7f6cc4744a9155e188cca73d2f6d1eb459dae
Brad Parks <bradleyparks@gmail.com>
2021-02-28 21:16:25 -0600

    after step 1

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,11 +18,20 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
@@ -30,7 +39,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyTheme {
-                MyApp()
+                NewsStory()
             }
         }
     }
@@ -38,9 +47,35 @@ class MainActivity : AppCompatActivity() {
 
 // Start building your app here!
 @Composable
-fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+fun NewsStory() {
+    MaterialTheme {
+        val typography = MaterialTheme.typography
+        Surface(color = MaterialTheme.colors.background) {
+            Column(modifier = Modifier.padding(16.dp)) {
+                Image(
+                    painterResource(id = R.drawable.header),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .height(180.dp)
+                        .fillMaxWidth()
+                        .clip(shape = RoundedCornerShape(20.dp)),
+                    contentScale = ContentScale.Crop
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Text(
+                    "A day wandering through the sandhills " +
+                            "in Shark Fin Cove, and a few of the " +
+                            "sights I saw",
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    style = typography.h6
+                )
+                Text("Davenport, California", style = typography.body2)
+                Text("December 2018", style = typography.body2)
+            }
+        }
     }
 }
 
@@ -48,7 +83,7 @@ fun MyApp() {
 @Composable
 fun LightPreview() {
     MyTheme {
-        MyApp()
+        NewsStory()
     }
 }
 
@@ -56,6 +91,6 @@ fun LightPreview() {
 @Composable
 fun DarkPreview() {
     MyTheme(darkTheme = true) {
-        MyApp()
+        NewsStory()
     }
 }
