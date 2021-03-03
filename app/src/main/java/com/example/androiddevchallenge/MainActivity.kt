addbc2a77ff8038ef9996f70718c1affe88e4868
Brad Parks <bradleyparks@gmail.com>
2021-03-03 00:51:46 -0600

    single import

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -22,7 +22,11 @@ import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -30,7 +34,11 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.TabRowDefaults
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -87,10 +95,12 @@ fun PetCard(
                 .background(color = backgroundColor)
                 .padding(10.dp)
                 .fillMaxWidth()
-                .clickable(onClick = {
-                    onPetClicked(pet)
-                    isSelected = !isSelected
-                })
+                .clickable(
+                    onClick = {
+                        onPetClicked(pet)
+                        isSelected = !isSelected
+                    }
+                )
         ) {
             Image(
                 painter = painterResource(pet.photo),
