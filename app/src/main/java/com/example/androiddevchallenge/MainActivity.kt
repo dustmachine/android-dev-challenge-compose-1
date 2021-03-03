8641f634f6da01c6678a2c67a60fee17b7f3bc77
Brad Parks <bradleyparks@gmail.com>
2021-03-02 23:00:54 -0600

    basic index list

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -19,61 +19,79 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.TabRowDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.androiddevchallenge.model.Pet
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyTheme {
-                NewsStory()
-            }
+            var pets = { "onepet" }
+            PetList(
+                pets = listOf(
+                    Pet("Russell", "a super doggo", R.drawable.russell),
+                    Pet("Steve", "a silly doggo", R.drawable.steve),
+                    Pet("Spike", "an energetic doggo", R.drawable.spike),
+                    Pet("Buddy", "a nice doggo", R.drawable.buddy),
+                    Pet("Morris", "a sweet doggo", R.drawable.morris)
+                )
+            )
+        }
+    }
+}
+
+@Composable
+fun PetList(pets: List<Pet>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(items = pets) { pet ->
+            PetCard(pet = pet)
+            TabRowDefaults.Divider(color = Color.Black, thickness = 3.dp)
         }
     }
 }
 
 // Start building your app here!
 @Composable
-fun NewsStory() {
-    MaterialTheme {
-        val typography = MaterialTheme.typography
-        Surface(color = MaterialTheme.colors.background) {
-            Column(modifier = Modifier.padding(16.dp)) {
-                Image(
-                    painterResource(id = R.drawable.header),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .height(180.dp)
-                        .fillMaxWidth()
-                        .clip(shape = RoundedCornerShape(20.dp)),
-                    contentScale = ContentScale.Crop
-                )
-
-                Spacer(modifier = Modifier.height(16.dp))
-
-                Text(
-                    "A day wandering through the sandhills " +
-                            "in Shark Fin Cove, and a few of the " +
-                            "sights I saw",
-                    maxLines = 2,
-                    overflow = TextOverflow.Ellipsis,
-                    style = typography.h6
-                )
-                Text("Davenport, California", style = typography.body2)
-                Text("December 2018", style = typography.body2)
+fun PetCard(
+    modifier: Modifier = Modifier,
+    pet: Pet
+) {
+    Surface(color = MaterialTheme.colors.background) {
+        Row(
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth()
+                .background(color = Color.White)
+        ) {
+            Image(
+                painter = painterResource(pet.photo),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(100.dp)
+                    .clip(shape = RoundedCornerShape(20.dp))
+            )
+            Column(
+                modifier = Modifier
+                    .padding(24.dp)
+            ) {
+                Text(text = "Name : ${pet.name}")
+                Text(text = "Breed : ${pet.breed}")
             }
         }
     }
@@ -83,14 +101,22 @@ fun NewsStory() {
 @Composable
 fun LightPreview() {
     MyTheme {
-        NewsStory()
+        PetList(
+            pets = listOf(
+                Pet("Russell", "a super doggo", R.drawable.russell),
+                Pet("Steve", "a silly doggo", R.drawable.steve),
+                Pet("Spike", "an energetic doggo", R.drawable.spike),
+                Pet("Buddy", "a nice doggo", R.drawable.buddy),
+                Pet("Morris", "a sweet doggo", R.drawable.morris)
+            )
+        )
     }
 }
-
-@Preview("Dark Theme", widthDp = 360, heightDp = 640)
-@Composable
-fun DarkPreview() {
-    MyTheme(darkTheme = true) {
-        NewsStory()
-    }
-}
+//
+//@Preview("Dark Theme", widthDp = 360, heightDp = 640)
+//@Composable
+//fun DarkPreview() {
+//    MyTheme(darkTheme = true) {
+//        NewsStory()
+//    }
+//}
---
+++ app/src/main/java/com/example/androiddevchallenge/model/Pet.kt
@@ -0,0 +1,5 @@
+package com.example.androiddevchallenge.model
+
+class Pet(val name: String, val breed: String, val photo: Int) {
+
+}
